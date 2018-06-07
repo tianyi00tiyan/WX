f0e1318b4798bb94d47c69c383f4953a8b47a561
t.zhou <t.zhou@vipshop.com>
2018-06-07 20:54:14 +0800

    u

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
-var express = require('express');
-// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
-var app = express();
+/* express的中间件 */
 
-// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
-// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
-// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
-// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+//1. 导入express
+var express = require('express')
 
-// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
-app.listen(3000, function () {
-  console.log('app is listening at port 3000');
-});
+//2. 创建express服务器
+var server = express()
+
+//3. 创建中间件:use
+//截取请求, 拦截回调
+server.use('/', function (request, response, next) {
+    console.log('执行中间件')
+    // console.log('获取数据库数据')
+    console.log(request.query.page)
+    next()
+})
+
+//4. 访问服务器(get或者post)
+//参数一: 请求根路径
+//4.1 get请求
+server.get('/home', function (request, response) {
+    // console.log(request)
+    response.send('get参数请求成功')
+})
+
+
+//5. 绑定端口
+server.listen(4040)
+console.log('启动4040')
+
